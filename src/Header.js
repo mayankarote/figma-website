00c75f0b5171f8840241ac243e651d6b6e181c09
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-05-30 18:56:07 +0530

    Updated

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+    return (
+        <div className="header">
+            <img className="header-logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+            <div className="header__searchInput" type="text"></div>
+            {/* logo */}
+
+        </div>
+    )
+}
