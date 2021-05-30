96a8aea1a63176b1b17f372cb6468e461f8f2230
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-05-30 19:39:22 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
-
+import "./App.css";
+import Header from "./Header";
 function App() {
   return (
     //BEM
     <div className="App">
       <h1>Lets Do it</h1>
-      {/* Header */}
+      <Header />
       {/* Home */}
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
 export default function Header() {
-    return (
-        <div className="header">
-            <img className="header-logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
-            <div className="header__searchInput" type="text"></div>
-            {/* logo */}
+  return (
+    <div className="header">
+      {/* Amazon name */}
+      <img
+        className="header-logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+      />
+      {/* search bar */}
+      <div className="header__searchInput" type="text"></div>
+      {/* logo */}
 
+      {/* Navbar */}
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
         </div>
-    )
+        <div className="header_option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+        <div className="header_option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+      </div>
+    </div>
+  );
 }
