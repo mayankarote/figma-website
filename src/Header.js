e565114b21663b83c91b18e3ef5e5ab9c17998a8
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-06-02 15:46:52 +0530

    Upaded

--- src/Header.js
+++ src/Header.js
@@ -23,7 +23,7 @@ export default function Header() {
           <span className="header__optionLineOne">Hello Guest</span>
           <span className="header__optionLineTwo">Sign In</span>
         </div>
-        <div className="header_option">
+        <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
           <span className="header__optionLineTwo">& Orders</span>
         </div>
