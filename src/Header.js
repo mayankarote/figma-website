6422a6e5931cab791ff09c4014b7897695d09f5e
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-06-02 15:57:54 +0530

    Upaded

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 export default function Header() {
   return (
@@ -31,6 +32,9 @@ export default function Header() {
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
+      </div>
+      <div className="header__optionBasket">
+        <ShoppingBasketIcon />
       </div>
     </div>
   );
