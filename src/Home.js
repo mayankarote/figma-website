d1ea302065828364c76587852e87689efeab7480
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-06-03 07:44:58 +0530

    Upaded

--- src/Home.js
+++ src/Home.js
@@ -13,18 +13,43 @@ function Home() {
         />
         <div className="home__row">
           <Product
+            id="12321341"
             title="The Lean Startup"
             price={24.99}
-            image={`https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg`}
             rating={3}
+            image={`https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg`}
           />
-          <Product />
+          <Product
+            id="49538094"
+            title="Kenwood kMix Stand Mixer for Baking, Stylish kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            price={239.0}
+            rating={4}
+            image={`https://images-na.ssl-images-amazon.com/images/I/810%2BGNdkzKL._AC_SX450_.jpg`}
+          />
         </div>
 
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            id="4903850"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+            price={199.99}
+            rating={3}
+            image={`https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg`}
+          />
+          <Product
+            id="23445930"
+            title="Amazon Echo (3rd Generation) | Smart Speaker with Alexa Charcol  Fabric"
+            price={98.99}
+            rating={5}
+            image={`https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOL_SLf?$300x400_retinamobilex2$`}
+          />
+          <Product
+            id="3254354345"
+            title="New Apple iPad Pro {12.9-inch, wi-fi, 128GB} - Silver {4th Generation}"
+            price={598.99}
+            rating={4}
+            image={`https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg`}
+          />
         </div>
         <div className="home__row">
           <Product />
