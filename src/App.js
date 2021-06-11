86daef009c651b70555cf3a5689986aed14b799e
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-06-11 12:05:43 +0530

    Figma-UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
-import Checkout from "./Checkout";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
@@ -12,15 +11,10 @@ function App() {
       <Router>
         <div className="App">
           <Switch>
-            <Route path="/checkout">
-              <Header />
-              <Checkout />
-            </Route>
             <Route path="/">
               <Header />
               <Home />
             </Route>
-            <Route path="" />
           </Switch>
         </div>
       </Router>
